4e64bb0daeb098531210ee4ce9799f2eb9e88bab
Simone Basso <bassosimone@gmail.com>
2011-09-16 18:19:33 +0200

    Improvements in the _per_city.py prototype

--- _per_city.py
+++ _per_city.py
@@ -16,57 +16,98 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+#
+# This is experimental code I'm using to produce per-city
+# results.
+#
+
 import json
 import matplotlib.pyplot
+import matplotlib.mlab
 import pprint
+import math
 import sys
+
+def __heavy_queue(stats, maximum):
+    nstats = []
+    for elem in stats:
+        if elem > maximum:
+            elem = maximum
+        nstats.append(elem)
+    return nstats
+
+def __per_city(cities, cityname, table, feature, scalefactor, bins,
+               cumulative=False, xrange=None, ext='svg'):
+    city = cities[cityname]
+
+    if feature in ('dload', 'upload'):
+        scaling = lambda value: value * 8.0 / (1000 * 1000)
+        xlabel = 'Bulk transfer rate for %s [Mbit/s]' % feature
+    elif feature == 'rtt':
+        scaling = lambda value: value * 1000.0
+        xlabel = 'Time to connect [ms]'
+    else:
+        raise ValueError('Invalid feature')
+
+    for isp in city.keys():
+        if not isp.startswith('AS30722') and not isp.startswith('AS1267') and not isp.startswith('AS12874') and not isp.startswith('AS3269'):
+            del city[isp]
+
+    side = int(math.sqrt(len(city))) + 1
+    index = 1
+    for isp, stats in city.items():
+        #matplotlib.pyplot.subplot(side, side, index)
+        matplotlib.pyplot.subplot(2, 2, index)
+        index = index + 1
+        stats = map(scaling, stats[table][feature])
+
+        if xrange:
+            stats = __heavy_queue(stats, xrange[1])
+
+        print(isp, len(stats))
+
+        if cumulative:
+            matplotlib.pyplot.hist(stats, bins, cumulative=True, range=xrange,
+                                   histtype='step', normed=True)
+            matplotlib.pyplot.ylim((0, 1.1))
+        else:
+            matplotlib.pyplot.hist(stats, bins, range=xrange, normed=True)
+
+        matplotlib.pyplot.grid(True, color='black')
+        matplotlib.pyplot.title(isp)
+        matplotlib.pyplot.ylabel('Frequency')
+        matplotlib.pyplot.xlabel(xlabel)
+
+    figure = matplotlib.pyplot.gcf()
+    figure.set_figheight(scalefactor * figure.get_figheight())
+    figure.set_figwidth(scalefactor * figure.get_figwidth())
+    if cumulative:
+        figure.savefig('%s-%s-%s_%s.%s' % (cityname, table, feature,
+                                           'cumulative', ext))
+    else:
+        figure.savefig('%s-%s-%s.%s' % (cityname, table, feature, ext))
+    figure.clear()
+
+#
+# Stuff for the paper.
+# Uncomment/comment to decide what to print.
+#
 
 filep = open(sys.argv[1], 'rb')
 cities = json.load(filep)
 filep.close()
-city = cities['Turin']
 
-#
-# The piece from now on is quite general and can
-# be reused for tool.py
-#
+#__per_city(cities, 'Turin', 'speedtest', 'rtt', scalefactor=4, bins=200,
+#           cumulative=False, xrange=(0, 200), ext='pdf')
+__per_city(cities, 'Turin', 'speedtest', 'rtt', scalefactor=4, bins=1000,
+           cumulative=True, xrange=(0, 200), ext='pdf')
 
-feature = 'upload'
-table = 'speedtest'
+#__per_city(cities, 'Turin', 'speedtest', 'dload', scalefactor=4, bins=20,
+#           cumulative=False, xrange=(0, 20), ext='pdf')
+#__per_city(cities, 'Turin', 'speedtest', 'dload', scalefactor=4, bins=1000,
+#           cumulative=True, xrange=(0, 20), ext='pdf')
 
-if feature == 'dload':
-    scaling = lambda value: value * 8.0 / (1000 * 1000)
-    xlabel = 'Measured download bulk transfer rate [Mbit/s]'
-    maximum = 100
-elif feature == 'upload':
-    scaling = lambda value: value * 8.0 / (1000 * 1000)
-    xlabel = 'Measured upload bulk transfer rate [Mbit/s]'
-    maximum = 100
-elif feature == 'rtt':
-    scaling = lambda value: value * 1000.0
-    xlabel = 'Time to connect [ms]'
-    maximum = 500
-
-#
-# Sort ISPs from the one with more tests to the one
-# with less tests.
-#
-isps = []
-for isp in cities['Turin']:
-    isps.append((len(city[isp][table][feature]), isp))
-isps = sorted(isps, reverse=True)
-
-for index in range(min(4, len(isps))):
-    matplotlib.pyplot.subplot(int("22%d" % (index + 1)))
-    isp = isps[index][1]
-    stats = city[isp][table][feature]
-    stats = map(scaling, stats)
-    ranges = None
-    if max(stats) > maximum:
-        ranges = (0, maximum)
-    matplotlib.pyplot.hist(stats, 100, ranges)
-    matplotlib.pyplot.title(isp)
-    matplotlib.pyplot.xlabel(xlabel)
-    matplotlib.pyplot.ylabel('Frequency')
-
-matplotlib.pyplot.show()
+#__per_city(cities, 'Turin', 'speedtest', 'upload', scalefactor=4, bins=20,
+#           cumulative=False, xrange=(0, 20), ext='pdf')
+#__per_city(cities, 'Turin', 'speedtest', 'upload', scalefactor=4, bins=1000,
+#           cumulative=True, xrange=(0, 1), ext='pdf')
