35a1514edeee08f2e62a14cebb8c3b23a418f327
Simone Basso <bassosimone@gmail.com>
2011-10-01 18:16:12 +0200

    data/count.py: count number of tests/users per day

---
+++ neubot/data/count.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+#
+# Copyright (c) 2011 Simone Basso <bassosimone@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+''' Counts number of users/tests per day '''
+
+import collections
+import getopt
+import pylab
+import sqlite3
+import sys
+import syslog
+
+def main():
+
+    ''' Counts number of users/tests per day '''
+
+    syslog.openlog('count.py', syslog.LOG_PERROR, syslog.LOG_USER)
+    count_users = False
+
+    try:
+        options, arguments = getopt.getopt(sys.argv[1:], 'u')
+    except getopt.error:
+        sys.exit('Usage: count.py [-u] file')
+    if len(arguments) != 1:
+        sys.exit('Usage: count.py [-u] file')
+
+    for tpl in options:
+        if tpl[0] == '-u':
+            count_users = True
+
+    connection = sqlite3.connect(arguments[0])
+    data = collections.defaultdict(list)
+    for table in ('speedtest', 'bittorrent'):
+        cursor = connection.cursor()
+        cursor.execute('SELECT timestamp, uuid FROM %s;' % table)
+        for result in cursor:
+            timestamp = int(pylab.epoch2num(result[0]))
+            data[timestamp].append(result[1])
+    connection.close()
+
+    xdata, ydata = [], []
+    for timestamp in sorted(data.keys()):
+        xdata.append(timestamp)
+        uuid_list = data[timestamp]
+        if count_users:
+            uuid_list = set(uuid_list)
+        ydata.append(len(uuid_list))
+
+    pylab.plot_date(xdata, ydata)
+    pylab.show()
+
+if __name__ == '__main__':
+    main()
